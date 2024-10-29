29ecb93de150b93a3def047e1964db4cfe297c69
trolololo22 <DAPavlov2@yandex.ru>
2024-10-29 21:44:25 +0300

    log

--- main.py
+++ main.py
@@ -24,4 +24,7 @@ async def main():
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except Exception as e:
+        logging.exception(e)
